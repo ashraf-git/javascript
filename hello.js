e94699b505e63602386086b482dc53e685893967
ashraf-git <aliashraf.hena@gmail.com>
2020-02-26 15:02:38 +0600

    number string boolean

--- hello.js
+++ hello.js
@@ -7,3 +7,30 @@ console.log(electricityBill);
 var mobileCharging = 34;
 console.log(mobileCharging);
 console.log(typeof mobileCharging);
+
+
+var name = "Mohammad Ali Ashraf khan";
+console.log(name);
+console.log(typeof name);
+
+var collage = "Comilla Govt. Collage";
+console.log(collage);
+console.log(typeof collage);
+
+var address = "Brahmanpar";
+console.log(address);
+console.log(typeof address);
+
+var shop = "nahar shop";
+console.log(shop);
+console.log(shop);
+
+var isHot = true;
+console.log(isHot);
+var sleep = false;
+
+console.log(sleep);
+
+var pass = true;
+console.log(pass);
+console.log(typeof pass);
