357ad865900a41c455bb1ca92570c17cb3b9cbf3
ashraf-git <aliashraf.hena@gmail.com>
2020-02-26 14:31:33 +0600

    variable

---
+++ hello.js
@@ -0,0 +1,9 @@
+var bananaPrice = 23;
+console.log(bananaPrice);
+var sunriseTime = 5;
+console.log(sunriseTime);
+var electricityBill = 300;
+console.log(electricityBill);
+var mobileCharging = 34;
+console.log(mobileCharging);
+console.log(typeof mobileCharging);
