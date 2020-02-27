a4c4f5da402d7477a00cbedc654b41212844fa39
ashraf-git <aliashraf.hena@gmail.com>
2020-02-27 09:56:35 +0600

    Function call Function

---
+++ function.js
@@ -0,0 +1,24 @@
+// function sayLoveYou(){
+//     console.log("I love you");
+//     console.log("Jan phone dai");
+// }
+// sayLoveYou();
+// var nums = 34;
+// console.log(nums);
+// sayLoveYou();
+
+// function doubleIt(num){
+//     var result = num * 2;
+//     return result;
+// }
+// var first = doubleIt(5);
+// var second = doubleIt(50);
+// var total = first + second;
+// console.log(total);
+
+function add(num1, num2){
+    var result =  num1 + num2;
+    return result;
+}
+var sum = add(4, 66);
+console.log(sum);
