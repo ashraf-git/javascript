098b3260a309658eb97d45ee415490bc8abfbb50
ashraf-git <aliashraf.hena@gmail.com>
2020-02-27 06:03:19 +0600

    FOR LOOP LOOP, RUN A LOOP FOR EACH ELEMENT OF AN ARRAY

---
+++ forloop.js
@@ -0,0 +1,16 @@
+
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// var nums = [34, 45, 54, 64, 53, 93, 56];
+// for(var i = 0; i < nums.length; i++){
+//     var element = nums[i];
+//     console.log(element);
+// }
+
+var friends = ['kamal', 'rana', 'soman'];
+for(var i = 0; i < friends.length; i++){
+    var element = friends[i];
+    console.log(element);
+}
